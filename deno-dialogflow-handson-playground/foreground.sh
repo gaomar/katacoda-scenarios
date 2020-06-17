93ba74267def937c39266c4aece13aa9a7c2b151
h-takauma <h-takauma@i-enter.co.jp>
2020-06-17 18:03:46 +0900

    denoハンズオン追加

---
+++ deno-dialogflow-handson-playground/foreground.sh
@@ -0,0 +1,15 @@
+sudo npm install -g n
+sudo n latest
+
+git clone https://github.com/gaomar/deno-dialogflowmessenger.git example
+
+cd example
+
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+source bash_profile
+
+clear
+
+echo "インストール完了しました！"
+
---
+++ deno-dialogflow-handson-playground/step1.md
@@ -0,0 +1,5 @@
+### 1-1. Denoを動かしてみよう
+下記コマンドを実行してみよう
+
+`deno run https://deno.land/std/examples/welcome.ts`{{execute}}
+
